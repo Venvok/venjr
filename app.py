a807b4b9464dc8b345e2a2255e359cf145206714
Venvok <112690144+Venvok@users.noreply.github.com>
2025-01-04 21:37:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -191,4 +191,4 @@ def create_account():
                 return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
